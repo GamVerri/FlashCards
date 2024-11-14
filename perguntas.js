82124ef230212c6e4a9d8dde5acb5e3a475cdc8d
GamVerri <168583062+GamVerri@users.noreply.github.com>
2024-11-14 20:31:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+   'Geografia',
+   'Qual o maior rio do mundo em volume de água',
+   'Rio Amazonas'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o dobro de 4',
+    '8'
+)
+
+criaCartao(
+    'Inglês',
+    'Como falamos amigo em Inglês',
+    'Friend'
+)
